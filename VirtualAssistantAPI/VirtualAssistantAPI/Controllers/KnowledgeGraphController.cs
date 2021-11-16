043a95597f3e04cb540c6eeb85c4a2a9e9889d73
Rasmus Hebsgaard <r.hebsgaard@gmail.com>
2021-11-16 13:22:26 +0100

    Added routes

---
+++ VirtualAssistantAPI/VirtualAssistantAPI/Controllers/KnowledgeGraphController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualAssistantBusinessLogic.KnowledgeGraph;
+using VirtualAssistantBusinessLogic.SparQL;
+namespace VirtualAssistantAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KnowledgeGraphController : ControllerBase
+    {
+        private readonly ILogger<KnowledgeGraphController> _logger;
+
+        public KnowledgeGraphController(ILogger<KnowledgeGraphController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[controller]/getNodes/{name}")]
+        public List<KnowledgeGraphNode> GetNodes(string name)
+        {
+            KnowledgeGraph graph = new KnowledgeGraph(new SparQLConnectionFactory());
+            var nodes = graph.FindNodes(name);
+            return nodes;
+        }
+
+        [HttpGet]
+        [Route("[controller]/getNode/{name}")]
+        public KnowledgeGraphNode GetNode(string name)
+        {
+            KnowledgeGraph graph = new KnowledgeGraph(new SparQLConnectionFactory());
+            var nodes = graph.FindNodes(name);
+            var node = graph.FindNodeInformation(nodes.FirstOrDefault());
+            return node;
+        }
+    }
+}
