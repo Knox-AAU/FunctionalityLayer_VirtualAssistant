c0d113189c2f5b1d72067e6888ca6e2b93e7f220
frejaz <fhpe19@student.aau.dk>
2021-11-26 10:04:54 +0100

    please

---
+++ VirtualAssistantBusinessLogic/VirtualAssistantBusinessLogicTests/SparQL/SparQLSelectTests.cs
@@ -0,0 +1,108 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using VirtualAssistantBusinessLogic.KnowledgeGraph;
+using VirtualAssistantBusinessLogic.SparQL;
+
+namespace VirtualAssistantBusinessLogicTests.SparQL
+{
+    [TestFixture]
+    public class SparQLSelectTests
+    {
+        private ISPOEncoder subSPOEncoder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.subSPOEncoder = Substitute.For<ISPOEncoder>();
+        }
+
+        private SparQLSelect CreateSparQLSelect()
+        {
+            return new SparQLSelect(
+                this.subSPOEncoder);
+        }
+
+        [Test]
+        public void Select_OneInputValue_SingleValueSavedCorrectly()
+        {
+            // Arrange
+            var sparQLSelect = this.CreateSparQLSelect();
+            string[] input = { "test" };
+
+
+            // Act
+            _ = sparQLSelect.Select(input);
+
+            // Assert
+            Assert.Contains(input[0], sparQLSelect.Selects);
+        }
+
+        [Test]
+        public void Select_700InputValue_AllValuesSavedCorrectly()
+        {
+            // Arrange
+            var sparQLSelect = this.CreateSparQLSelect();
+            List<string> input = new();
+
+            for (int i = 0; i < 700; i++)
+            {
+                input.Add(i.ToString());
+            }
+
+            // Act
+            foreach(string s in input){
+                sparQLSelect.Select(s);
+            }
+
+            // Assert
+            foreach(string s in input)
+            {
+                Assert.Contains(s, sparQLSelect.Selects);
+            }
+        }
+
+        [Test]
+        public void Where_SelectsAreZero_ThrowsCountZeroException()
+        {
+            // Arrange
+            var sparQLSelect = this.CreateSparQLSelect();
+
+            // Act // Assert
+            Assert.Throws<CountZeroException>(() => sparQLSelect.Where());
+        }
+
+        [Test]
+        public void Where_SelectsHasValues_CorrectObjectReturned()
+        {
+            // Arrange
+            var sparQLSelect = this.CreateSparQLSelect();
+            sparQLSelect.Select("Test1", "Test2", "Test3");
+            SparQLWhere actual = sparQLSelect.Where();
+
+            // Act // Assert
+            Assert.IsTrue(actual.SparQLSelect.Equals(sparQLSelect));
+        }
+
+        [Test]
+        public void ToString_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var sparQLSelect = this.CreateSparQLSelect();
+            EncodedSPO output = new ($"?s1 ?p \"SUBJECT\"@en . ", $"?s1");
+            sparQLSelect.SPOEncoder
+                .EncodeSubject(Arg.Any<string>())
+                .Returns(output);
+            string expected = "SELECT ?s1 ?VALUELabel ";
+
+            // Act
+            sparQLSelect.From("SUBJECT");
+            sparQLSelect.Select("VALUE");
+            string actual = sparQLSelect.ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ VirtualAssistantBusinessLogic/VirtualAssistantBusinessLogicTests/SparQL/SparQLWhereTests.cs
@@ -0,0 +1,114 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using VirtualAssistantBusinessLogic.KnowledgeGraph;
+using VirtualAssistantBusinessLogic.SparQL;
+
+namespace VirtualAssistantBusinessLogicTests.SparQL
+{
+    [TestFixture]
+    public class SparQLWhereTests
+    {
+        private SparQLSelect subSparQLSelect;
+        private ISPOEncoder subSPOEncoder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            EncodedSPO output = new($"?s1 ?p \"SUBJECT\"@en . ", $"?s1");
+            subSPOEncoder = Substitute.For<ISPOEncoder>();
+            subSPOEncoder
+                .EncodeSubject(Arg.Any<string>())
+                .Returns(output);
+
+            subSparQLSelect = Substitute.For<SparQLSelect>(subSPOEncoder);
+        }
+
+        private SparQLWhere CreateSparQLWhere()
+        {
+            return new SparQLWhere(
+                this.subSparQLSelect);
+        }
+
+        [Test]
+        public void SubjectIs_HasEncodedSPO_SubjectStringCorrect()
+        {
+            // Arrange
+            SparQLWhere sparQLWhere = this.CreateSparQLWhere();
+            sparQLWhere.EncodedSPOs.Add("TEST", new EncodedSPO("TRIPLET", "NAME"));
+            string subject = "TEST";
+
+
+            // Act
+            _ = sparQLWhere.SubjectIs(
+                subject);
+
+            // Assert
+            Assert.AreEqual("NAME", sparQLWhere.SubjectString);
+        }
+
+        [Test]
+        public void SubjectIs_NoEncodedSPO_SubjectStringCorrect()
+        {
+            // Arrange
+            SparQLWhere sparQLWhere = this.CreateSparQLWhere();
+            string subject = "TEST";
+
+
+            // Act // Assert
+            Assert.Throws<KeyNotFoundException>(
+                () => sparQLWhere.SubjectIs(subject)
+                );
+        }
+
+        [Test] 
+        public void PredicateIs_HasEncodedSPO_PredicateStringCorrect()
+        {
+            // Arrange
+            SparQLWhere sparQLWhere = this.CreateSparQLWhere();
+            sparQLWhere.EncodedSPOs.Add("PREDICATE", new EncodedSPO("TRIPLET", "NAME"));
+            string predicate = "PREDICATE";
+
+
+            // Act
+            _ = sparQLWhere.PredicateIs(
+                predicate);
+
+            // Assert
+            Assert.AreEqual("NAME", sparQLWhere.PredicateString);
+        }
+
+        [Test] 
+        public void PredicateIs_NoEncodedSPO_PredicateStringCorrect()
+        {
+            // Arrange
+            SparQLWhere sparQLWhere = this.CreateSparQLWhere();
+            string predicate = "PREDICATE";
+
+
+            // Act // Assert
+            Assert.Throws<KeyNotFoundException>(
+                () => sparQLWhere.PredicateIs(predicate)
+                );
+        }
+
+        [Test]
+        public void ObjectIs_HasEncodedSPO_PredicateStringCorrect()
+        {
+            // Arrange
+            SparQLWhere sparQLWhere = this.CreateSparQLWhere();
+            sparQLWhere.EncodedSPOs.Add("TEST", new EncodedSPO("TRIPLET", "NAME"));
+            string obj = "OBJ";
+
+
+            // Act
+            _ = sparQLWhere.ObjectIs(
+                obj);
+
+            // Assert
+            Assert.AreEqual("?OBJ", sparQLWhere.ObjectString);
+        }
+
+    }
+}
